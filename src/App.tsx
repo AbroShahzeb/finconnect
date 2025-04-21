05cf07ed2102c23b819f07acc8bbb898930e2c41
AbroShahzeb <khizr772@gmail.com>
2025-04-21 14:50:08 +0500

    Setup Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="text-3xl font-bold flex items-center justify-center w-full min-h-screen">
+      Hello World
+    </div>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['finconnect.shahzebabro.com', 'www.finconnect.shahzebabro.com', 'https://finconnect.shahzebabro.com', 'https://www.finconnect.shahzebabro.com']
-  }
-})
+    allowedHosts: [
+      "finconnect.shahzebabro.com",
+      "www.finconnect.shahzebabro.com",
+      "https://finconnect.shahzebabro.com",
+      "https://www.finconnect.shahzebabro.com",
+    ],
+  },
+});
