8a47e01748e5400d0085f636540f9287affa6042
AbroShahzeb <khizr772@gmail.com>
2025-04-21 15:37:17 +0500

    Setup Design system and variables for my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-3xl font-bold flex items-center justify-center w-full min-h-screen">
+    <div className="bg-surface text-3xl font-bold flex items-center justify-center w-full min-h-screen">
       Hello World
     </div>
   );
