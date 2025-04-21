8ea7030514f1664fa241e4b03b612fad246e85d7
AbroShahzeb <khizr772@gmail.com>
2025-04-21 14:34:47 +0500

    Set Allowed Hosts for Deployed in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['finconnect.shahzebabro.com', 'www.finconnect.shahzebabro.com', 'https://finconnect.shahzebabro.com', 'https://www.finconnect.shahzebabro.com']
+  }
 })
